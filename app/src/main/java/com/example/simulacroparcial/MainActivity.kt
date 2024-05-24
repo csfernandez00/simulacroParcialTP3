8dd786f85db246ea06f66e7f89ab789ebce2ed7a
Carlos Fernandez <csfernandez00@gmail.com>
2024-05-24 12:39:19 -0300

    Push inicial

---
+++ app/src/main/java/com/example/simulacroparcial/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.example.simulacroparcial
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val navHostFrag = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+
+        val bottomNavView = findViewById<BottomNavigationView>(R.id.bottomNavigation)
+
+        NavigationUI.setupWithNavController(bottomNavView,navHostFrag.navController)
+
+    }
+
+}
